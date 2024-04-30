08f8967e6fb62f9df7cedd5b562223bad398864d
thong-dipro <thongtd@dipro.vn>
2024-04-30 22:39:36 +0700

    chore: edit message

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gif = qs(".gif");
 const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
 
 const handleYesClick = () => {
-  question.innerHTML = "Yeahhhhhhhhhhh! See you tomorrow!!";
+  question.innerHTML = "Yeahhhhhhhhhhh! See you in Friday night!!";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 
   // Remove the 'mouseover' event listener from noBtn
@@ -140,7 +140,7 @@ const handleYesClick = () => {
     const randomIndex = Math.floor(Math.random() * dateIdeas.length);
     const selectedDateIdea = dateIdeas[randomIndex];
 
-    alert(`How about this romantic date idea: ${selectedDateIdea}`);
+    alert(`Bạn được mời đi đánh cầu lông tối thứ 6 tuần này!!!`);
   });
 
   // Replace yesBtn with the new letsGoBtn
